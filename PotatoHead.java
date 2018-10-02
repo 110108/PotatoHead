da2c8fc09ed54e69f0dc84435ac43eceff84400d
Aiden71601 <37307697+Aiden71601@users.noreply.github.com>
2018-10-02 09:59:19 -0400

    Added and changed methods
    Added the getShoes method and setCostume method
    
    Changed the setEyeColor, getEyeColor and toString methods
    
    Added some new instance variables

--- PotatoHead.java
+++ PotatoHead.java
@@ -1,12 +1,18 @@
 // hacktoberfest 2018
+import java.util.Scanner;
+
 public class PotatoHead
 {
+	Scanner scan = new Scanner(System.in);
 	// create instance variables
 	private boolean hasEyes;
 	private String eyeColor;
 	private boolean hasMustache;
 	// add one instance variable
 	private boolean hasShoes;
+	private String costume;
+	private String someCostume;
+	private String someColor;
 
 	// default constructor
 	public PotatoHead()
@@ -16,6 +22,7 @@ public class PotatoHead
 		eyeColor = "unknown";
 		hasMustache = false;
 		hasShoes = false;
+		costume = "unknown";
 	}
 
 	/*
@@ -26,16 +33,30 @@ public class PotatoHead
 	// accessor method for eye color
 	public String getEyeColor()
 	{
-		return eyes;
+		return eyeColor;
+	}
+
+	// accessor method for hasShoes
+	public boolean getHasShoes()
+	{
+		return hasShoes;
 	}
 
 	// void setEyeColor(String someColor)
 	// MUTATOR METHOD - change an instance variable's value
 	public void setEyeColor(String someColor)
 	{
+		System.out.print("What eye color do you want: ");
+		someColor = scan.nextLine();
 		eyeColor = someColor;
 	}
 
+	public void setCostume()
+	{
+		System.out.print("What costume do you want: ");
+		someCostume = scan.nextLine();
+		costume = someCostume;
+	}
 	// String toString()
 	// method to access ***ALL*** instance variables
 	// you should be adding your new instance variable here
@@ -43,9 +64,9 @@ public class PotatoHead
 	public String toString()
 	{
 		String output;
-		output =  "Eyes: " + eyes;
-		output += "\nMustache: " + mustache;
-		output += "\nShoes: " + shoe;
+		output =  "Eyes: " + eyeColor;
+		output += "\nMustache: " + hasMustache;
+		output += "\nShoes: " + hasShoes;
 		output += "\nCostume: " + costume;
 		return output;
 	}
