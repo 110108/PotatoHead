c701bedfcb2a3cc7d09c386d03d888b17826c21a
ohiofi <20hz20khz@gmail.com>
2018-10-02 00:14:07 -0400

    init

---
+++ PotatoHead.java
@@ -0,0 +1,49 @@
+// hacktoberfest 2018
+public class PotatoHead
+{
+	// create instance variables
+	private boolean hasEyes;
+	private String eyeColor;
+	private boolean hasMustache;
+	// add one instance variable
+
+	// default constructor
+	public PotatoHead()
+	{
+		// initialize ***ALL*** instance variables
+		hasEyes = false;
+		eyeColor = "unknown";
+		hasMustache = false;
+	}
+
+	/*
+	VARIOUS ACCESSOR AND MUTATOR METHODS IN ALPHABETICAL ORDER
+	*/
+
+	// String getEyeColor()
+	// accessor method for mustache
+	public String getEyeColor()
+	{
+		return eyes;
+	}
+
+	// void setEyeColor(String someColor)
+	// MUTATOR METHOD - change an instance variable's value
+	public void setEyeColor(String someColor)
+	{
+		eyeColor = someColor;
+	}
+
+	// String toString()
+	// method to access ***ALL*** instance variables
+	// you should be adding your new instance variable here
+	// ACCESSOR METHOD
+	public String toString()
+	{
+		String output;
+		output =  "Eyes: " + eyes;
+		output += "\nMustache: " + mustache;
+		output += "\nCostume: " + costume;
+		return output;
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # PotatoHead
+
+How to contribute:
+
+- Add one instance variable
+- Initialize ***ALL*** instance variables
+- Add one or more methods related to your attribute
+- Add your instance variable to the toString() method
