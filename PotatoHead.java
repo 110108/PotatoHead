1d0eb4acfddb52271c5e453a6521b8e4b5311e39
110108 <31996737+110108@users.noreply.github.com>
2018-10-03 07:41:04 -0400

    added arm bool and int + fixed some other stuff

--- PotatoHead.java
+++ PotatoHead.java
@@ -3,8 +3,10 @@ public class PotatoHead
 {
 	// create instance variables
 	private boolean hasGlasses;
+	private boolean haveArms;
 	private boolean hasEyes;
-	private int numberOfEyes;
+	private int armNum;
+	private int eyeNum;
 	private String eyeColor;
 	private boolean hasMustache;
 	private boolean hasCostume;
@@ -16,18 +18,38 @@ public class PotatoHead
 	{
 		// initialize ***ALL*** instance variables
 		hasGlasses = false;
+		haveArms = false;
 		hasEyes = false;
-		numberOfEyes = 0;
+		armNum = 0;
+		eyeNum = 0;
 		eyeColor = "unknown";
 		hasMustache = false;
 		hasCostume = true;
-		hasPhone = true;
+		hasPhone = false;
 	}
 
 	/*
 	VARIOUS ACCESSOR AND MUTATOR METHODS IN ALPHABETICAL ORDER
 	*/
 
+
+	//mutator method for adding eyes
+	public void addEye(int num)
+	{
+		eyeNum += num;
+	}
+
+	// accesor method for number of arms
+	public int getNumOfArms()
+	{
+		return armNum;
+	}
+
+	// accesor method for arms
+	public boolean hasArms()
+	{
+		return haveArms;
+	}
 
 	// accessor method for number of eyes
 	public int getNumOfEyes()
@@ -40,13 +62,13 @@ public class PotatoHead
 	{
 		return eyeColor;
 	}
-	
+
 	//accessor method for glasses
 	public boolean haveGlasses()
 	{
 		return hasGlasses;
 	}
-	
+
 	// void setEyeColor(String someColor)
 	// MUTATOR METHOD - change an instance variable's value
 	public void setEyeColor(String someColor)
@@ -55,10 +77,6 @@ public class PotatoHead
 	}
 
 
-	public void addEye(int num)
-	{
-		eyeNum += num;
-	}
 
 	public void takeEye(int num)
 	{
@@ -71,13 +89,13 @@ public class PotatoHead
 			System.out.println("Sorry, but this Potatohead has no eyes to take away!");
 		}
 	}
-	
+
 	//mutator method to put on or take off glasses
 	public void setHasGlasses(boolean wantGlasses)
 	{
 		hasGlasses = wantGlasses;
 	}
-	
+
 	// String toString()
 	// method to access ***ALL*** instance variables
 	// you should be adding your new instance variable here
@@ -85,7 +103,9 @@ public class PotatoHead
 	public String toString()
 	{
 		String output;
-		output =  "Glasses: " + glasses;
+		output =  "Glasses: " + hasGlasses;
+		output += "\nArms: " + haveArms;
+		output += "\nNumber of arms: " + armNum;
 		output += "\nNumber of eyes: "+ eyeNum;
 		output =  "\nEyes: " + hasEyes;
 		output += "\nMustache: " + hasMustache;
